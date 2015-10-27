42d3b4e5cd3d7d902afa927266aabe6dd5f8cc22
lucho9292 <luiscoronel186@gmail.com>
2015-10-27 00:07:47 -0300

    Apariencia de Tile

--- src/main/java/bomber/Tile.java
+++ src/main/java/bomber/Tile.java
@@ -1,12 +1,16 @@
 package bomber;
 
+import java.awt.Color;
+
 import com.uqbar.vainilla.GameComponent;
+import com.uqbar.vainilla.appearances.Rectangle;
 
 public class Tile extends GameComponent<BombermanGameScene> {
 	
 	private boolean rompible = false;
 	
 	public Tile(boolean rompible, double x, double y){
+		this.setAppearance(new Rectangle(Color.DARK_GRAY, 30, 30));
 		this.rompible = rompible;
 		this.setX(x);
 		this.setY(y);
