850a7ab4d3cee57fc0e4676603467a54764a8ca2
lucho9292 <luiscoronel186@gmail.com>
2015-10-26 23:58:52 -0300

    cambio en el movimiento en la clase BombermanPlayer

--- src/main/java/bomber/BombermanPlayer.java
+++ src/main/java/bomber/BombermanPlayer.java
@@ -11,25 +11,26 @@ import java.awt.Color;
 public class BombermanPlayer extends GameComponent<BombermanGameScene> {
 	
 	public BombermanPlayer(Color c){
-		this.setAppearance(new Rectangle(c, 28, 65));
+		this.setAppearance(new Rectangle(c, 30, 30));
 		//this.setAppearance(Sprite.fromImage("\\png\\ff3warrior.png").scale(0.8));
 		this.setX(200);
 		this.setY(200);
+		
 	}
 	
 	//Keyboard movements
 	public void update(DeltaState deltaState) {
-		if (deltaState.isKeyBeingHold(Key.LEFT)){
-			this.setX(this.getX()-1);
+		if (deltaState.isKeyPressed(Key.LEFT)){
+			this.setX(this.getX()-30);
 		}
-		if (deltaState.isKeyBeingHold(Key.RIGHT)){
-			this.setX(this.getX()+1);
+		if (deltaState.isKeyPressed(Key.RIGHT)){
+			this.setX(this.getX()+30);
 		}
-		if (deltaState.isKeyBeingHold(Key.UP)){
-			this.setY(this.getY()-1);
+		if (deltaState.isKeyPressed(Key.UP)){
+			this.setY(this.getY()-30);
 		}
-		if (deltaState.isKeyBeingHold(Key.DOWN)){
-			this.setY(this.getY()+1);
+		if (deltaState.isKeyPressed(Key.DOWN)){
+			this.setY(this.getY()+30);
 		}
 	}
 
