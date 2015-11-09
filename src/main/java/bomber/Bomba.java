77d267a0797bf7e479ae3f8d01716c5fd9fe6364
lucho9292 <luiscoronel186@gmail.com>
2015-11-08 23:11:24 -0300

    Agregado el metodo spawnFire() en la clase Bomba. Ahora se generan 5 instancias de Fuego cuando explota la bomba

--- src/main/java/bomber/Bomba.java
+++ src/main/java/bomber/Bomba.java
@@ -20,9 +20,24 @@ public class Bomba extends GameComponent<BombermanGameScene> {
 	}
 	
 	public void explotar(){
+		
+		this.spawnFire();
 		this.destroy();
 		//avisar al jugador que exploto
 		//this.spawnFire(this.x,this.y)
+	}
+	
+	public void spawnFire(){
+		Fuego f1 = new Fuego(this.getX(),this.getY());
+		Fuego f2 = new Fuego(this.getX()+30,this.getY());
+		Fuego f3 = new Fuego(this.getX()-30,this.getY());
+		Fuego f4 = new Fuego(this.getX(),this.getY()+30);
+		Fuego f5 = new Fuego(this.getX(),this.getY()-30);
+		this.getScene().addComponent(f1);
+		this.getScene().addComponent(f2);
+		this.getScene().addComponent(f3);
+		this.getScene().addComponent(f4);
+		this.getScene().addComponent(f5);
 	}
 	
 	public void update(DeltaState ds){
--- src/main/java/bomber/Fuego.java
+++ src/main/java/bomber/Fuego.java
@@ -1,11 +1,28 @@
 package bomber;
 
+import java.awt.Color;
+
+import com.uqbar.vainilla.DeltaState;
 import com.uqbar.vainilla.GameComponent;
+import com.uqbar.vainilla.appearances.Circle;
 
 public class Fuego extends GameComponent<BombermanGameScene>{
+	private double elapsed = 0.0;
 	private double countdown = 0.6;
-			
+	
+	public Fuego(double x, double y){
+		this.setX(x);
+		this.setY(y);
+		this.setAppearance(new Circle(Color.RED, 20));
+	}
+	
 	public void destruirTiles(){
 		//TODO
-	}	
+	}
+	
+	public void update(DeltaState ds){
+		this.elapsed += ds.getDelta();
+		if (elapsed >= countdown)
+			{this.destroy();}
+	}
 }
