e3038ddc8f43f5c3b2c0986e9ec218790a31a80a
lucho9292 <luiscoronel186@gmail.com>
2015-11-10 23:52:25 -0300

    Manejo de tiles abstracto. Ahora se pueden crear "paredes" hechas de tiles

--- src/main/java/bomber/BombermanGameScene.java
+++ src/main/java/bomber/BombermanGameScene.java
@@ -12,7 +12,6 @@ public class BombermanGameScene extends GameScene {
 	private Pared pared2 = new Pared(false,0,0);
 	private Pared pared3 = new Pared(true,770,0);
 	private Pared pared4 = new Pared(false,0,550);
-	//tiles
 	//monstruos
 	
 	public BombermanGameScene(){
@@ -21,6 +20,26 @@ public class BombermanGameScene extends GameScene {
 		this.addComponent(pared2);
 		this.addComponent(pared3);
 		this.addComponent(pared4);
+		this.createVerticalWall(4, 4, 10);
+		this.createHorizontalWall(6,6,2);
+	}
+	
+	public void createVerticalWall(double x, double y,int cant){
+		Tile firstTile = new Tile(false,x,y);
+		this.addComponent(firstTile);
+		for(int i=1; i<=cant; i++){
+			Tile newTile = new Tile(false,x,y+i);
+			this.addComponent(newTile);
+		}
+	}
+	
+	public void createHorizontalWall(double x, double y,int cant){
+		Tile firstTile = new Tile(false,x,y);
+		this.addComponent(firstTile);
+		for(int i=1; i<=cant; i++){
+			Tile newTile = new Tile(false,x+i,y);
+			this.addComponent(newTile);
+		}
 	}
 	
 }
--- src/main/java/bomber/Tile.java
+++ src/main/java/bomber/Tile.java
@@ -5,15 +5,15 @@ import java.awt.Color;
 import com.uqbar.vainilla.GameComponent;
 import com.uqbar.vainilla.appearances.Rectangle;
 
-public class Tile extends GameComponent<BombermanGameScene> {
+public class Tile extends RichGameComponent {
 	
 	private boolean rompible = false;
 	
 	public Tile(boolean rompible, double x, double y){
 		this.setAppearance(new Rectangle(Color.DARK_GRAY, 30, 30));
 		this.rompible = rompible;
-		this.setX(x);
-		this.setY(y);
+		this.setX(x*rate);
+		this.setY(y*rate);
 	}
 	
 	public boolean esRompible(){
