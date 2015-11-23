944ee1b4e13c0b0e3b3b07aa3a480ae3dce642a7
Diego54 <d.caminos54@gmail.com>
2015-11-23 15:47:44 -0300

    me falto agregar los cambios en la pared

---
+++ src/main/java/bomber/SolidBlock.java
@@ -0,0 +1,16 @@
+package bomber;
+
+import java.awt.Color;
+
+import com.uqbar.vainilla.GameComponent;
+import com.uqbar.vainilla.appearances.Rectangle;
+
+public class SolidBlock extends GameComponent<BombermanGameScene>{
+	
+	public SolidBlock(int x, int y){
+		this.setAppearance(new Rectangle(Color.BLACK, 53, 46));
+		this.setX(x);
+		this.setY(y);
+	}
+
+}
