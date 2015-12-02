5f5d53f5c6204f111cd065dd36b3076479459627
lucho9292 <luiscoronel186@gmail.com>
2015-12-02 00:16:38 -0300

    SolidBlock ahora hereda de RichGameComponent

--- src/main/java/bomber/SolidBlock.java
+++ src/main/java/bomber/SolidBlock.java
@@ -2,15 +2,14 @@ package bomber;
 
 import java.awt.Color;
 
-import com.uqbar.vainilla.GameComponent;
 import com.uqbar.vainilla.appearances.Rectangle;
 
-public class SolidBlock extends GameComponent<BombermanGameScene>{
+public class SolidBlock extends RichGameComponent{
 	
 	public SolidBlock(int x, int y){
 		//Apariencia de grid
 		//this.setAppearance(new TileAppearrence<Tile>());
-		this.setAppearance(new Rectangle(Color.BLACK, 53, 46));
+		this.setAppearance(new Rectangle(Color.BLACK, w, h));
 		this.setX(x);
 		this.setY(y);
 	}
